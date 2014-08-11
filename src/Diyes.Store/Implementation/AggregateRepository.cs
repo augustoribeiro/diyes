6aa6a523b857e0c184243ae8bfeed153cf325a86
Augusto Ribeiro Silva <ars@d60.dk>
2014-08-11 22:26:24 +0200

    extracted method

--- src/Diyes.Store/Implementation/AggregateRepository.cs
+++ src/Diyes.Store/Implementation/AggregateRepository.cs
@@ -22,22 +22,28 @@ namespace Diyes.Store.Implementation
             Store = store;
         }
 
-        public virtual T Load<T>(IIdentity aggregateId) where T : AbstractAggregate
+        public T Load<T>(IIdentity aggregateId) where T : AbstractAggregate
         {
             var eventStream = Store.LoadEventStream(aggregateId);
             var typeT = typeof(T);
-            var ctor = typeT.GetConstructor(
-                BindingFlags.NonPublic | BindingFlags.CreateInstance | BindingFlags.Instance,
-                null, new[] {typeof (EventStream)}, null);
-
-            if (ctor == null)
-                throw new InvalidOperationException("this should never happen since we demand that T is subclass of AbstractAggregate");
+            var ctor = GetAggregateConstructor<T>(typeT);
 
             var instance = (T) ctor.Invoke(new object[] { eventStream });
 
             return instance;
         }
 
-        
+        private static ConstructorInfo GetAggregateConstructor<T>(Type typeT) where T : AbstractAggregate
+        {
+            var ctor = typeT.GetConstructor(
+                BindingFlags.NonPublic | BindingFlags.CreateInstance | BindingFlags.Instance,
+                null, new[] {typeof (EventStream)}, null);
+
+            if (ctor == null)
+                throw new InvalidOperationException(
+                    "this should never happen since we demand that T is subclass of AbstractAggregate");
+
+            return ctor;
+        }
     }
 }
