15b75a16e9357d9c95fcac36ae2f62652cdad98f
Augusto Ribeiro Silva <ars@d60.dk>
2014-08-16 22:23:29 +0200

    starting on an Example and Sql store tests

---
+++ src/Diyes.BankExample/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Diyes.Store.Implementation;
+using Diyes.Store.Interfaces;
+
+namespace Diyes.BankExample
+{
+    public class BankAccount : AbstractAggregate
+    {
+        protected BankAccount(EventStream eventStream) : base(eventStream)
+        {
+        }
+
+        public void Open()
+        {
+            if (IsOpen)
+            {
+                throw new BankAccountException("This account was already open");
+            }
+
+            Apply(new AccountOpened(Id));
+        }
+
+        public bool IsOpen { get; private set; }
+    }
+
+    public class AccountOpened : Event
+    {
+        public AccountOpened(IIdentity id)
+        {
+            AggregateId = id;
+        }
+
+        public IIdentity AggregateId { get; private set; }
+    }
+
+    public class BankAccountException : Exception
+    {
+        public BankAccountException(string thisAccountWasAlreadyOpen)
+        {
+            
+        }
+    }
+}
---
+++ src/sqltest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Diyes.Sql;
+using Diyes.Store.Implementation;
+using Diyes.Store.Interfaces;
+using Diyes.Test;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "server=.;initial catalog=EventStore;integrated security=sspi";
+            var sqlStore = new SqlAppendOnlyStore(connectionString);
+
+            var aggregateRep = new AggregateRepository(new EventStore(sqlStore));
+
+            var aggregateId = new Identity(Guid.NewGuid());
+            var concrete = aggregateRep.Load<ConcreteAggregate>(aggregateId);
+
+            concrete.Create();
+
+            aggregateRep.Save(concrete);
+
+            var task = new TaskFactory();
+
+            var stopwatch = new Stopwatch();
+
+
+
+            task.StartNew(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    var concreteAggregate = aggregateRep.Load<ConcreteAggregate>(aggregateId);
+                    Random random = new Random();
+                    var number = random.Next(1, 42);
+                    concreteAggregate.ChangeNumber(number);
+                    try
+                    {
+                        aggregateRep.Save(concreteAggregate);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            });
+
+            task.StartNew(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    var concreteAggregate = aggregateRep.Load<ConcreteAggregate>(aggregateId);
+                    Random random = new Random();
+                    var number = random.Next(1, 42);
+                    concreteAggregate.ChangeNumber(number);
+                    try
+                    {
+                        aggregateRep.Save(concreteAggregate);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            });
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
